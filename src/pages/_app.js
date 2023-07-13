112f6d1f3ffa22ae138264c853d0dcb51fd61c9f
Alexandra Haynes <sandamunteanua@yahoo.com>
2023-07-13 11:44:57 -1000

    add favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,10 +10,9 @@ export default function App({ Component, pageProps }) {
           name="viewport"
           content="width=device-width, initial-scale=1.0"
         ></meta>
+
         <link rel="icon" href="/favicon.ico" />
         <title>Discover Romania</title>
-
-       
       </Head>
       <Component {...pageProps} />
     </>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,9 +4,17 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true'/>
-       <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300&family=Quicksand:wght@500&display=swap" rel="stylesheet"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+      
+        <link
+          href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300&family=Quicksand:wght@500&display=swap"
+          rel="stylesheet"
+        />
         {/* Bootstrap */}
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
