3a61749e4e389086d1bd78847bfc556199d51d27
Alexandra Haynes <sandamunteanua@yahoo.com>
2023-07-14 16:36:49 -1000

    add social icons for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,23 @@
 import React from "react";
 import Link from "next/link";
+import {
+  AiFillYoutube,
+  AiFillInstagram,
+  AiFillFacebook,
+  AiFillTwitterCircle
+} from 'react-icons/ai'
 
 const Footer = () => {
   return (
-    <footer className="w-full bg-primaryBrown text-white pt-12">
+    <footer className="w-full max-w-screen bg-slate-900 text-white pt-12">
       <div
         className="flex flex-col items-center gap-2 
       justify-center text-sm xl:text-xl pt-6  "
       >
-        <Link href="/privacy-policy" className="hover:-translate-y-1 transition-all ease-in-out ">
+        <Link
+          href="/privacy-policy"
+          className="hover:-translate-y-1 transition-all ease-in-out "
+        >
           Privacy Policy
         </Link>
         <Link href="/terms-of-use" className="hover:-translate-y-1">
@@ -18,6 +27,25 @@ const Footer = () => {
       <p className="text-sm xl:text-xl font-semibold text-center py-4">
         Copyright © Discover Romania 2023
       </p>
+
+<div className="w-1/2 h-[1px] mx-auto bg-slate-300/80">
+
+</div>
+      <div className="flex items-center justify-around pt-6 pb-12 w-1/2 mx-auto text-slate-400 ">
+        
+        <Link href="instagram.com">
+          <AiFillInstagram />
+        </Link>
+        <Link href="instagram.com">
+          <AiFillYoutube />
+        </Link>
+        <Link href="instagram.com">
+          <AiFillFacebook />
+        </Link>
+        <Link href="instagram.com">
+          <AiFillTwitterCircle />
+        </Link>
+      </div>
     </footer>
   );
 };
