62ae4a4c4cbdc83af0eb13dca244dfd1a728c520
Alexandra Haynes <sandamunteanua@yahoo.com>
2023-07-15 15:46:50 -1000

    add forum hero section

--- src/pages/forum.js
+++ src/pages/forum.js
@@ -1,20 +1,82 @@
-import CommentList from '@/components/CommentList'
+import Link from "next/link";
+import CommentList from "@/components/CommentList";
+import Post from "@/components/Post";
+import React, { useEffect, useState } from "react";
 
+export async function fetchPosts() {
+  const res = await fetch("http://localhost:3000/api/post", {
+    cache: "no-store",
+  });
 
-import React from 'react'
-
-const forum = () => {
-   
-   
-
-  return (
-    <div>
-      
-     
-        <CommentList />
-    
-    </div>
-  );
+  return res.json();
 }
 
-export default forum
+export default function Forum() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPostsData = async () => {
+      try {
+        const data = await fetchPosts();
+        setPosts(data);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPostsData();
+  }, []);
+
+  return (
+    <>
+      <main className="max-w-screen-sm m-auto">
+        <section>
+          <div className="flex items-center bg-white">
+            <div className="container   flex flex-col items-center justify-between px-6 py-8 mx-auto">
+              <div className="flex flex-col">
+                <h1 className="w-full text-4xl font-light text-center text-gray-800 uppercase sm:text-5xl">
+                  Welcome to the forum
+                </h1>
+                <h2 className="w-full max-w-2xl py-8 mx-auto text-xl font-light text-center text-gray-500">
+                  A place to come and share your thoughts about Romania, read
+                  interesting stories and recommendations. Feel free to post
+                  comments.
+                </h2>
+                <div className="flex items-center justify-center mt-4 gap-x-5">
+                  <Link
+                    href="/"
+                    className="border-1 bg-orange-300 border-primary px-6 py-2 rounded-md text-white font-semibold hover:bg-primary hover:text-white transition-all duration-300"
+                  >
+                    Featured Post
+                  </Link>
+                  <Link
+                    href="/"
+                    className="border-1 bg-orange-300 border-primary px-6 py-2 rounded-md text-white font-semibold hover:bg-primary hover:text-white transition-all duration-300"
+                  >
+                    Our Users
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        {loading ? (
+          <h3>Loading posts...</h3>
+        ) : (
+          <>
+            {posts.length > 0 && <h2 className="text-center">Forum</h2>}
+            {posts.length > 0 ? (
+              posts.map((post) => <Post key={post._id} post={post} />)
+            ) : (
+              <h3>No posts available.</h3>
+            )}
+          </>
+        )}
+      </main>
+    </>
+  );
+}
